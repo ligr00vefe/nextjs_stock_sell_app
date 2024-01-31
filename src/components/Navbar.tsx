103a0d3d5f12d1f6b08259774fcd74868bd835b9
ligr00vefe <ligr00vefe@naver.com>
2024-02-01 02:57:20 +0900

    layout, NavItem 복구

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,20 @@
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import NavItem from './NavItem';
-// import SearchBox from './SearchBox';
+import SearchBox from './SearchBox';
 import { User } from '@prisma/client';
 import { useRouter } from 'next/navigation';
-import getCurrentUser from '@/app/actions/getCurrentUser';
 
 interface NavbarProps {
   // 유저가 로그인 되어서 props로 user 데이터를 받아왔을 때는 Prisma/client에서 제공하는 기본 User 안의 타입을 쓰고 로그인이 안되었을 때는 타입 null 
   currentUser?: User | null; 
 }
 
-const Navbar = () => {
+const Navbar = ( { currentUser }: NavbarProps ) => {
+  // console.log('currentUser', currentUser);
 
   const [menu, setMenu] = useState(false);
   const router = useRouter();
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [currentPath, setCurrentPath] = useState('');
 
   const handleMenu = () => {
@@ -26,29 +25,18 @@ const Navbar = () => {
 
   useEffect(() => {
     // 페이지 로드 시에도 경로 업데이트
-    // setCurrentPath(window.location.pathname);
+    setCurrentPath(window.location.pathname);
 
-    const checkUserStatus = async () => {
-      const user = await getCurrentUser();
-      setCurrentUser(user);
-      console.log('user', user);
-
-      // 현재 URL 확인
-      const currentUrl = window.location.pathname; 
-
-      // 현재 경로가 로그인 페이지가 아니고, 사용자가 로그인되어 있지 않은 경우에만 리다이렉트
-      if (!user && currentUrl  !== '/api/auth/signin') {
-        router.push('/api/auth/signin'); // 로그인 페이지 경로
-      }
-    };
-
-    checkUserStatus();
+    // 만약 로그인이 되어 있지 않다면 로그인 페이지로 리다이렉트합니다.
+    if (!currentUser) {
+      router.push('/api/auth/signin'); // 로그인 페이지 경로
+    } 
 
     // console.log('router', router);
     // console.log('currentUser', currentUser);
     // console.log('Current Path:', currentPath);
 
-  }, []);
+  }, [currentUser, currentPath, router]);
 
   return (
     <nav className='relative z-10 w-full bg-blue-500 text-white py-2'>
