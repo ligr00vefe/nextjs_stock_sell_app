b1eb4a172df30bae0627a0a661e4149113de34ae
ligr00vefe <ligr00vefe@naver.com>
2024-01-31 23:39:35 +0900

    next.config.js 내용 삭제

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 기존 구성에 옵션을 추가합니다.
-  // output: "export" 또는 "standalone"으로 설정해야 함
-  output: {
-    // output 옵션 설정
-    export: true
-  }
+  // 필요한 다른 구성 옵션들
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
