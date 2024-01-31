21099109e16727d2f919f207a3e6d625b2c6ec36
ligr00vefe <ligr00vefe@naver.com>
2024-01-31 23:22:53 +0900

    next.config.js 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 기존 구성에 옵션을 추가합니다.
+  output: {
+    // 정적 HTML을 내보냅니다.
+    export: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
