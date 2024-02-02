34327ebeb012d2f4915d8a2149fe40e6fd5e2bdc
ligr00vefe <ligr00vefe@naver.com>
2024-02-03 04:39:37 +0900

    Navbar 무한 로딩 수정6

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,8 +38,8 @@ const Navbar = () => {
       }
   
       // 만약 로그인이 되어 있지 않다면 로그인 페이지로 리다이렉트합니다.
-      if (!userSession || !user) {
-        if (!window.location.pathname.startsWith('/auth/login')) {
+      if (!userSession && !user) {
+        if (!window.location.pathname.startsWith('/auth/login') && !window.location.pathname.startsWith('/api/auth/signin')) {
           router.push('/api/auth/signin'); // 로그인 페이지 경로
         }
       }   
