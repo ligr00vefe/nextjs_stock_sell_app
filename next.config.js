a43ebafeef6f18d70d57acc1be15cf8029eda139
ligr00vefe <ligr00vefe@naver.com>
2024-02-04 07:11:19 +0900

    .env 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 필요한 다른 구성 옵션들
-  publicRuntimeConfig: {
-    // NEXTAUTH_URL 값을 런타임에서 접근할 수 있게 설정
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 }
 
 
