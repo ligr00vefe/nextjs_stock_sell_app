92ec0bfae8e78a3af5ea7f8605d109f797aa390a
ligr00vefe <ligr00vefe@naver.com>
2024-02-05 03:08:02 +0900

    Navbar console.log 수정

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,8 +28,10 @@ const Navbar = () => {
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        const response = await axios.get(`${process.env.NEXTAUTH_PUBLIC_URL}/api/user`);
+        const response = await axios.get('/api/user');
         console.log('response_currentUser', response.data);
+        console.log('NEXTAUTH_URL', process.env.NEXTAUTH_URL);
+        console.log('NEXTAUTH_PUBLIC_URL', process.env.NEXTAUTH_PUBLIC_URL);
 
         setCurrentUser(response.data);
       } catch (error) {
