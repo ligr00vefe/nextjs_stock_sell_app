ebc17b6c9a08b7d4ef74983c1ec6d04d07f038a2
ligr00vefe <ligr00vefe@naver.com>
2024-02-06 03:42:08 +0900

    Netlify 배포 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 필요한 다른 구성 옵션들
-  
+  reactStrictMode: true,
+  env: {
+      NEXT_URL: process.env.NEXT_PUBLIC_URL
+    }
 }
 
 
