5258ea5e3941743df0c6c10aad4d2a9455501762
ligr00vefe <ligr00vefe@naver.com>
2024-02-06 22:22:53 +0900

    getCurrentUser 함수 복구

--- src/app/actions/getCurrentUser.ts
+++ src/app/actions/getCurrentUser.ts
@@ -1,6 +1,5 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import prisma from "@/helpers/prismadb";
 
 export async function getSession() {
   return await getServerSession(authOptions);
@@ -18,7 +17,7 @@ export default async function getCurrentUser() {
     // console.log('session.user.email: ', session.user.email);
 
     // 이메일을 이용해서 데이터베이스에서 요청 정보 찾은 후 가져오기
-    const currentUser = await prisma.user.findUnique({
+    const currentUser = await prisma?.user.findUnique({
       where: {
         email: session.user.email
       }
