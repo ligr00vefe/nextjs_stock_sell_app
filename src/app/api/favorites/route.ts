2644e3fc0a7096f72c89ee64e9f0a106e074cba9
ligr00vefe <ligr00vefe@naver.com>
2024-02-14 06:47:12 +0900

    favorites/route 간소화

--- src/app/api/favorites/route.ts
+++ src/app/api/favorites/route.ts
@@ -1,21 +1,55 @@
 import { NextResponse } from "next/server";
-import getFavorites from "@/app/actions/getFavorites";
+import prisma from "@/helpers/prismadb";
+import { getSession } from "@/app/actions/getCurrentUser";
 
 // 즐겨찾기 가져오기
 // GET 요청 처리
 export async function GET() {
   try {
-    // 데이터 가져오기
-    const resultData = await getFavorites();
+    const currentSession = await getSession();
+    console.log('favorites_currentSession', currentSession);
+    
+    let query: any = {};
+    
+    if (currentSession?.user?.id) {
+      query.userId = currentSession.user.id;
+    }
+
+    if (currentSession?.user?.favoriteIds) {
+      const user = await prisma.user.findUnique({
+        where: { id: currentSession.user.id },
+        select: { favoriteIds: true }
+      });
+
+      if (user) {
+        query.stockId = { in: user.favoriteIds };
+      }
+    }
+
+    const favorites = await prisma.favorite.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
+      },
+    });
+
+    const totalItems = await prisma.favorite.count({
+      where: query
+    });
+
+    const resultData = {
+      data: favorites,
+      currentSession,
+      totalItems
+    };
+
     console.log('favorites_route_resultData: ', resultData);
-    // console.log('favorites_route_resultData.data: ', resultData.data);
-    // console.log('favorites_route_resultData.currentSession: ', resultData.currentSession);
-    // console.log('favorites_route_resultData.totalItems: ', resultData.totalItems);
 
     // 성공적인 응답 반환
     return NextResponse.json({ resultData });
   } catch (error) {
     // 오류 처리
-    return NextResponse.json({ error: 'Error fetching stocks' });
+    console.error('Error fetching favorites:', error);
+    return NextResponse.json({ error: 'Error fetching favorites' });
   }
 }
