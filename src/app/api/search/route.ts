84f3dd5e7794c1169b65ea4c0f6bf34ac6e2d157
ligr00vefe <ligr00vefe@naver.com>
2024-02-17 09:00:39 +0900

    /search/route error 타입 추가

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -48,6 +48,6 @@ export async function GET(req: NextRequest) {
     }
 
   } catch (error) {
-    return NextResponse.json({ error: `Failed to fetch current price: ${error.message}` });
+    return NextResponse.json({ error: `Failed to fetch current price: ${(error as Error).message}` });
   }
 }
