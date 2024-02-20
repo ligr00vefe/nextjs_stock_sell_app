0c738c9107f64f64d5cd198bcb18c360e03d9733
ligr00vefe <ligr00vefe@naver.com>
2024-02-20 22:36:32 +0900

    FavorietesPage, SellsPage api user호출 부분 수정

--- src/app/api/sells/route.ts
+++ src/app/api/sells/route.ts
@@ -1,20 +1,66 @@
 import { NextResponse } from 'next/server';
-import getSellStocks from '@/app/actions/getSellStocks';
+import prisma from "@/helpers/prismadb";
+import getCurrentUser from '@/app/actions/getCurrentUser';
 
 // GET 요청 처리
 export async function GET() {
   try {
     // 데이터 가져오기
-    const resultData = await getSellStocks();
-    // console.log('sellStocks_route_resultData: ', resultData);
-    // console.log('sellStocks_route_resultData.data: ', resultData.data);
-    // console.log('sellStocks_route_resultData.currentSession: ', resultData.currentSession);
-    // console.log('sellStocks_route_resultData.totalItems: ', resultData.totalItems);
+    const currentUser = await getCurrentUser();
+    // console.log('sellStock_currentUser', currentUser);
+    
+    let query: any = {};
+          
+    if (currentUser?.id) {
+      // 현재 사용자의 ID로 favorite 테이블에서 해당 사용자의 데이터만 가져옵니다.
+      query.userId = currentUser.id;
+    }
+
+    if (currentUser?.favoriteIds) {
+      // 사용자 ID로 사용자의 favoriteIds 필드에 포함된 주식만 가져오도록 수정
+      const user = await prisma.user.findUnique({
+        where: { id: currentUser.id },
+        select: { favoriteIds: true }
+      });
+
+      if (user) {
+        query.stockId = { in: user.favoriteIds };
+      }
+    }   
+
+    // 테이블의 데이터를 여러개 가져올 때 findMany() 사용    
+    const favorites = await prisma.favorite.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
+      },
+    })
+
+    const newFavorites = favorites.filter(favorite => {
+      if (favorite.price !== null && favorite.price > 0 && favorite.desired_selling_price !== null && favorite.desired_selling_price > 0) {
+        // favorite.price가 favorite.desired_selling_price보다 크거나 같은지를 체크하여 필터링
+        return favorite.price >= favorite.desired_selling_price;
+      }
+      return false;
+    });
+    
+    const totalItems = await prisma.favorite.count({
+      where: query
+    });
+    
+    const resultData = {
+      data: newFavorites,
+      currentUser,
+      totalItems
+    }
+
+    // console.log('favorites_route_resultData: ', resultData);
 
     // 성공적인 응답 반환
     return NextResponse.json({ resultData });
   } catch (error) {
     // 오류 처리
+    console.error('Error fetching sellStocks:', error);
     return NextResponse.json({ error: 'Error fetching stocks' });
   }
 }
