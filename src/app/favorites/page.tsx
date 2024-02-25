08f53e994aad23614415ae2b2f9dc096cd721661
ligr00vefe <ligr00vefe@naver.com>
2024-02-25 22:51:29 +0900

    favoritesPage 수정5

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,12 +1,26 @@
 
 import React from 'react'
 import getCurrentUser from '../actions/getCurrentUser';
+import getFavorites from '../actions/getFavorites';
 
 export default async function FavoritesPage() {
-      const currentUser = await getCurrentUser();
-      console.log('favoritesPage_currentUser', currentUser);
+      const currentUser1 = await getCurrentUser();
+      console.log('favoritesPage_currentUser', currentUser1);
+      const {data, currentUser, totalItems} = await getFavorites();
   return (
-    <div>favoritesPage</div>    
+    <div>
+      <h1>favoritesPage</h1>   
+      <h2>{totalItems}</h2>   
+     
+      {data && data.map((stock) => (
+              <div key={stock.id}>
+                <p>{stock.id}</p>
+                <p>{stock.symbol}</p>
+                <p>{stock.company}</p>
+                <p>{stock.currency}</p>
+              </div>
+            ))}
+    </div>    
   )
   
 }
